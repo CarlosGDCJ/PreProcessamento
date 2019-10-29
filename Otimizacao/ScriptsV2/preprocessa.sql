0996e217dae3b26a7a0fd2935c585590a3bfc396
Carlos <carlosgdcj@hotmail.com>
2019-10-28 22:15:06 -0300

    Segunda iteração da otimização

---
+++ Otimizacao/ScriptsV2/preprocessa.sql
@@ -0,0 +1,161 @@
+DROP TABLE IF EXISTS UnifTeste;
+CREATE TABLE UnifTeste (
+    t_rec TEXT,
+    rx NUMERIC,
+    rvaluemin NUMERIC,
+    rvaluemax NUMERIC,
+    rvalueavg NUMERIC,
+    usfluxmin NUMERIC,
+    usfluxmax NUMERIC,
+    usfluxavg NUMERIC,
+    classemin TEXT,
+    classemax TEXT
+);
+
+DROP INDEX IF EXISTS unif_idx;
+CREATE INDEX unif_idx ON UnifTeste(t_rec);
+
+DROP FUNCTION IF EXISTS preProcessa;
+CREATE FUNCTION preProcessa ( tabela regclass ) RETURNS VOID AS $$
+DECLARE 
+    temp TEXT;
+    rvmin NUMERIC;
+    rvmax NUMERIC;
+    rvavg NUMERIC;
+    ufmin NUMERIC;
+    ufmax NUMERIC;
+    ufavg NUMERIC;
+    axray1 NUMERIC;
+    axray2 NUMERIC;
+    axray3 NUMERIC;
+    axray4 NUMERIC;
+    axray5 NUMERIC;
+    axray6 NUMERIC;
+    axray7 NUMERIC;
+    axray8 NUMERIC;
+    axray9 NUMERIC;
+    axray10 NUMERIC;
+    axray11 NUMERIC;
+    axray12 NUMERIC;
+    clmin TEXT;
+    clmax TEXT;
+    curs refcursor;
+BEGIN
+    TRUNCATE TABLE UnifTeste;
+    OPEN curs FOR EXECUTE format('SELECT DISTINCT t_rec FROM %s ORDER BY t_rec', tabela);
+    LOOP
+        FETCH curs INTO temp;
+        EXIT WHEN NOT FOUND;
+
+        EXECUTE format('
+            CREATE OR REPLACE VIEW Intervalo AS 
+            SELECT t_rec, noaa_ar, xray1 , xray2 , xray3 , xray4 , xray5 , xray6 , xray7 , xray8 , xray9 , xray10 , xray11, xray12, r_value, usflux, class 
+            FROM %s WHERE ''%s'' LIKE t_rec
+            ', tabela, temp);
+
+        SELECT MIN(NULLIF(NULLIF(r_value, 0), 'NaN')) FROM Intervalo INTO rvmin;
+        SELECT MAX(NULLIF(NULLIF(r_value, 0), 'NaN')) FROM Intervalo INTO rvmax;
+        SELECT AVG(NULLIF(NULLIF(r_value, 0), 'NaN')) FROM Intervalo INTO rvavg;
+
+        SELECT MIN(usflux) FROM Intervalo INTO ufmin;
+        SELECT MAX(usflux) FROM Intervalo INTO ufmax;
+        SELECT AVG(usflux) FROM Intervalo INTO ufavg;
+
+        SELECT MIN(class) FROM Intervalo INTO clmin;
+        SELECT MAX(class) FROM Intervalo INTO clmax;
+
+        SELECT xray1, xray2, xray3, xray4, xray5, xray6, xray7, xray8, xray9, xray10, xray11, xray12 
+        FROM Intervalo LIMIT 1 
+        INTO axray1, axray2, axray3, axray4, axray5, axray6, axray7, axray8, axray9, axray10, axray11, axray12;
+
+
+        IF (temp LIKE '%00:00_TAI') THEN
+
+            INSERT INTO UnifTeste (t_rec, rx, rvaluemin, rvaluemax, rvalueavg, usfluxmin, usfluxmax, usfluxavg, classemin, classemax) 
+            VALUES  (temp, axray1, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '01:00_TAI', axray2, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '02:00_TAI', axray3, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '03:00_TAI', axray4, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '04:00_TAI', axray5, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '05:00_TAI', axray6, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '06:00_TAI', axray7, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '07:00_TAI', axray8, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '08:00_TAI', axray9, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '09:00_TAI', axray10, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '10:00_TAI', axray11, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '11:00_TAI', axray12, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax);
+
+        ELSIF (temp LIKE '%12:00_TAI') THEN
+            INSERT INTO UnifTeste (t_rec, rx, rvaluemin, rvaluemax, rvalueavg, usfluxmin, usfluxmax, usfluxavg, classemin, classemax) 
+            VALUES  (temp, axray1, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '13:00_TAI', axray2, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '14:00_TAI', axray3, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '15:00_TAI', axray4, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '16:00_TAI', axray5, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '17:00_TAI', axray6, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '18:00_TAI', axray7, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '19:00_TAI', axray8, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '20:00_TAI', axray9, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '21:00_TAI', axray10, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '22:00_TAI', axray11, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '23:00_TAI', axray12, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax);
+            
+        ELSIF (temp LIKE '%24:00_TAI') THEN
+            INSERT INTO UnifTeste (t_rec, rx, rvaluemin, rvaluemax, rvalueavg, usfluxmin, usfluxmax, usfluxavg, classemin, classemax) 
+            VALUES  (temp, axray1, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '25:00_TAI', axray2, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '26:00_TAI', axray3, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '27:00_TAI', axray4, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '28:00_TAI', axray5, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '29:00_TAI', axray6, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '30:00_TAI', axray7, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '31:00_TAI', axray8, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '32:00_TAI', axray9, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '33:00_TAI', axray10, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '34:00_TAI', axray11, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '35:00_TAI', axray12, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax);
+        ELSIF (temp LIKE '%36:00_TAI') THEN
+            INSERT INTO UnifTeste (t_rec, rx, rvaluemin, rvaluemax, rvalueavg, usfluxmin, usfluxmax, usfluxavg, classemin, classemax) 
+            VALUES  (temp, axray1, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '37:00_TAI', axray2, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '38:00_TAI', axray3, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '39:00_TAI', axray4, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '40:00_TAI', axray5, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '41:00_TAI', axray6, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '42:00_TAI', axray7, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '43:00_TAI', axray8, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '44:00_TAI', axray9, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '45:00_TAI', axray10, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '46:00_TAI', axray11, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '47:00_TAI', axray12, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax);
+        ELSIF (temp LIKE '%48:00_TAI') THEN
+            INSERT INTO UnifTeste (t_rec, rx, rvaluemin, rvaluemax, rvalueavg, usfluxmin, usfluxmax, usfluxavg, classemin, classemax) 
+            VALUES  (temp, axray1, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '49:00_TAI', axray2, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '50:00_TAI', axray3, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '51:00_TAI', axray4, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '52:00_TAI', axray5, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '53:00_TAI', axray6, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '54:00_TAI', axray7, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '55:00_TAI', axray8, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '56:00_TAI', axray9, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '57:00_TAI', axray10, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '58:00_TAI', axray11, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax),
+                    (substring(temp, 1, 14) || '59:00_TAI', axray12, rvmin, rvmax, rvavg, ufmin, ufmax, ufavg, clmin, clmax);
+        END IF;
+    END LOOP;
+    CLOSE curs;
+
+END;
+$$ LANGUAGE plpgsql;
+
+
+/* Template de função
+DROP FUNCTION nomeFuncao;
+CREATE FUNCTION nomeFuncao ( nomeParam tipoParam ) RETURNS tipoRetorno AS $$
+DECLARE nomeVarLocal tipoVarLocal;
+BEGIN
+    
+END;
+$$ LANGUAGE plpgsql;
+*/
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ solares
 * Execute os scripts aqui do repositório e acesse as tabelas criadas por eles
 
 ## Scripts
-### preProc1
+### preProc1/
 #### **funcao.sql** (nome temporário)
 Contém as functions responsáveis por converter a tabela *solarflare_sharp_720s_BDunificado* no formato desejado. 
     
@@ -18,7 +18,7 @@ Contém as functions responsáveis por converter a tabela *solarflare_sharp_720s
 
     Cria a tabela **UnifTeste** e preenche ela com os valores de tempo com granularidade de **minutos** insere também o valor de **raio x** medido nesse instante, e a **média, mínimo e máximo para os valores de usflux e r_value**. O nome da tabela passada como parâmetro deve ser colocado entre aspas simples.
     
-### preProc2
+### preProc2/
 #### **criaTabelaTeste.sql**
 Contém as functions responsáveis por converter a tabela *solarflare_sharp_720s_BDunificado* no formato desejado, gerando a tabela Teste. 
     
@@ -37,9 +37,19 @@ Contém as functions responsáveis por adicionar colunas de rvalue e usflux à t
     
     Atualiza a tabela passado como parâmetro, adicionando colunas de usflux e rvalue para cada uma das diferentes regiões ativas (noaa_ar) contidas na tabela. Além disso, retira as colunas de noaa_ar, rvalue(geral) e usflux(geral). O nome da tabela passada como parâmetro deve ser colocado entre aspas simples.
 
-### Otimizacao
-Essa pasta contém os scripts que, se tudo der certo, rodarão na base toda em tempo aceitável
-#### funcmin.sql
-Mesma abordagem do funcao.sql, mas utiliza índice e menos SQL dinâmico. A *function* preprocessa() agora não recebe argumento nenhum. Quando executada vai pegar a tabela de nome **unifmin** e deixar no formato desejado.
-#### funcminDel.sql
-Mesma abordagem do funcmin.sql, mas deleta as entradas da tabela original depois de serem utilizadas. **Altera a tabela que é passada**
+### Otimizacao/
+Essa pasta contém os scripts que, se tudo der certo, rodarão na base toda em tempo aceitável.
+### Sugestoes.txt
+Arquivo com melhorias/estratégias que foram ou serão aplicadas.
+#### ScriptsV1/
+Primeira iteração do processo de otimização. Melhorias incluem uso de índice e redução do SQL dinâmico.
+##### funcmin.sql
+Mesma abordagem do funcao.sql, mas utiliza índice e menos SQL dinâmico. A function preprocessa() agora não recebe argumento nenhum. Quando executada vai pegar a tabela de nome **unifmin** e deixar no formato desejado.
+##### funcminDel.sql
+Mesma abordagem do funcmin.sql, mas deleta as entradas da tabela original a medida que são utilizadas. **Altera a tabela que é passada**
+#### ScriptsV2/
+Segunda iteração da otimização. Conta com um modelo mais eficiente de manipulação dos dados dos raios-x e menos conversões (cast). Reduzir o SQL dinâmico não alterou em nada o desempenho, então ele retorna nessa versão para facilitar os teste.
+##### preprocessa.sql
+Trabalhamos com uma tabela que agora tem mais de um tipo de dado, buscando aumento de desepenho no cálculo das funções de agregação. A function preprocessa volta a receber como argumento o nome da tabela a ser processada.
+##### preprocessaDel.sql
+Mesma abordagem do preprocessa.sql, mas deleta as entradas da tabela passada a medida que são utilizadas. **Altera a tabela passada**
