aeca0fdd252cf3351c2efe551007bb87d0a8977a
Carlos <carlosgdcj@hotmail.com>
2019-11-11 19:23:41 -0300

    Resultados iniciais - primeira tentativa de classificação

---
+++ Continuidade/importaPorAnoTexto.sql
@@ -0,0 +1,9 @@
+\copy continua FROM '/home/carlos/Downloads/TabelaContinuaCompletaTexto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2010 FROM '/home/carlos/Downloads/continua2010Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2011 FROM '/home/carlos/Downloads/continua2011Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2012 FROM '/home/carlos/Downloads/continua2012Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2013 FROM '/home/carlos/Downloads/continua2013Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2014 FROM '/home/carlos/Downloads/continua2014Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2015 FROM '/home/carlos/Downloads/continua2015Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2016 FROM '/home/carlos/Downloads/continua2016Texto.csv' DELIMITER ',' CSV HEADER;
+\copy continua2017 FROM '/home/carlos/Downloads/continua2017Texto.csv' DELIMITER ',' CSV HEADER;
